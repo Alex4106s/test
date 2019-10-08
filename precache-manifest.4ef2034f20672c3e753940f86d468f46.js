6dae80e5765937f79d31d8aaa4255239802959aa
Alex4106s <shuragilyow1@gmail.com>
2019-10-09 02:51:21 +0300

    Updates

---
+++ precache-manifest.4ef2034f20672c3e753940f86d468f46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538b77c5c2d3ef8000b99c86dba22b6e",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "3dac49672fd611b2b652",
+    "url": "/test/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "9d7147cb405befc6eadd",
+    "url": "/test/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "3dac49672fd611b2b652",
+    "url": "/test/static/js/2.b41d1068.chunk.js"
+  },
+  {
+    "revision": "9d7147cb405befc6eadd",
+    "url": "/test/static/js/main.238696c8.chunk.js"
+  },
+  {
+    "revision": "a8583e497ed1118e4092",
+    "url": "/test/static/js/runtime-main.a756f8be.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/test/static/media/persik.4e1ec840.png"
+  }
+]);
