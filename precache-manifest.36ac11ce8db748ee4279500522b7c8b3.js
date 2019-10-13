e826ea70e01a4f9c205ccf843f7c22d5b1e0edf9
Alex4106s <shuragilyow1@gmail.com>
2019-10-13 06:19:43 +0300

    Updates

---
+++ precache-manifest.36ac11ce8db748ee4279500522b7c8b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "3e1b5948e7d1d4014592",
+    "url": "/test/static/js/main.c80b499d.chunk.js"
+  },
+  {
+    "revision": "ed9a0030612bd9805980",
+    "url": "/test/static/js/2.288ae0a7.chunk.js"
+  },
+  {
+    "revision": "3e1b5948e7d1d4014592",
+    "url": "/test/static/css/main.411d7bc6.chunk.css"
+  },
+  {
+    "revision": "ed9a0030612bd9805980",
+    "url": "/test/static/css/2.779f109e.chunk.css"
+  },
+  {
+    "revision": "689613ff4e0249d2827b2fd05effa402",
+    "url": "/test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.0a58678128d30bfc91be19be9dfe41fb.js"
+  "/test/precache-manifest.36ac11ce8db748ee4279500522b7c8b3.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
+workbox.routing.registerNavigationRoute("/test/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
